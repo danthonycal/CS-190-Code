caa8fad059effde49db2d327c5f379236fc31bd2
danthonycal <dacalixto@up.edu.ph>
2021-06-03 13:15:58 +0800

    -

--- vidToFrame.py
+++ vidToFrame.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 def get_vid_paths():
 	paths = []
 	dynamic_path = os.getcwd()
-	path = dynamic_path+"\\videos"
+	path = dynamic_path+"/videos"
 	for subdir, dirs, files in os.walk(path):
 		for filename in files:
 			filepath = subdir + os.sep + filename
@@ -27,8 +27,8 @@ def vidToFrame(vid_file, count, success):
 	vid_id = vid_file[-14:-4]
 	vid_frame_folder = f"{vid_id}-frames"
 
-	if not os.path.exists(f"{os.getcwd()}\\frames\\{vid_frame_folder}"):
-		os.makedirs(f"{os.getcwd()}\\frames\\{vid_frame_folder}")
+	if not os.path.exists(f"{os.getcwd()}/frames/{vid_frame_folder}"):
+		os.makedirs(f"{os.getcwd()}/frames/{vid_frame_folder}")
 
 	vid_cap = cv2.VideoCapture(vid_file)
 	vid_cap.set(cv2.CAP_PROP_POS_FRAMES, 160)
@@ -52,10 +52,10 @@ def vidToFrame(vid_file, count, success):
 			continue
 		# if (count%fps==0):
 		success, frame = vid_cap.read()
-		output_path = f"\\frames\\{vid_frame_folder}\\{vid_id}-{count}.png"
+		output_path = f"/frames/{vid_frame_folder}/{vid_id}-{count}.png"
 		save_path = f"/frames/{vid_frame_folder}/{vid_id}-{count}.png\n"
 		file_loc.append(save_path)
-		plt.imsave(f"{os.getcwd()}\\{output_path}", frame, cmap=plt.cm.gray)
+		plt.imsave(f"{os.getcwd()}/{output_path}", frame, cmap=plt.cm.gray)
 		printProgressBar(i + 1, total_files, prefix='Progress:', suffix='Complete', length=50)
 		i+=1
 		count += 1
@@ -73,8 +73,8 @@ def processVidToFrame():
 	frame_name = "frame"
 	vid_paths = get_vid_paths()
 
-	if not os.path.exists(f'{args.res_path}\\frames'):
-		os.makedirs(f'{args.res_path}\\frames')
+	if not os.path.exists(f'{args.res_path}/frames'):
+		os.makedirs(f'{args.res_path}/frames')
 
 	print(vid_paths)
 	for vid_path in vid_paths:
