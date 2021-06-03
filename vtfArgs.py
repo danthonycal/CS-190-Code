ea9f4475a6f8648a39fa14326dcd0ca68d98e17b
danthonycal <dacalixto@up.edu.ph>
2021-06-03 13:05:34 +0800

    Added result path argument to vtfArgs.py

--- vtfArgs.py
+++ vtfArgs.py
@@ -4,6 +4,8 @@ class VidToFrameArgs(object):
 	def __init__(self):
 		parser = argparse.ArgumentParser(description="VidToFrame Arguments")
 		parser.add_argument("--file_end", dest="file_end", help="\".MP4\" or \"8008.MP4\"", default=".MP4", type=str)
+		parser.add_argument("--res_path", dest="res_path", help="Path to result", default="", type=str)
 
 		args = parser.parse_args()
 		self.file_end = args.file_end
+		self.res_path = args.res_path
